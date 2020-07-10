cfaf431239a9af465c044db42ad44fbbb4c2ed9e
JakobSchauser <jakob.schauser@gmail.com>
2020-07-10 22:44:53 +0200

    Made cursor

---
+++ game/cursor.gd
@@ -0,0 +1,25 @@
+extends KinematicBody2D
+
+
+# Declare member variables here. Examples:
+# var a = 2
+# var b = "text"
+
+onready var lastpos = get_viewport().get_mouse_position()
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	#Input.set_mouse_mode(Input.MOUSE_MODE_CONFINED)
+	Input.set_mouse_mode(Input.MOUSE_MODE_HIDDEN)
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _physics_process(delta):
+	position = get_viewport().get_mouse_position()
+
+func left():
+	get_viewport().warp_mouse(Vector2(0,0))
+
+func _input(event):
+	if Input.is_action_just_pressed("ui_accept"):
+		left()
